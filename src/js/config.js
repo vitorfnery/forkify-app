fdcd80f5ea5fb5987048e6ad9ea30df6cdc63e5b
Vítor F. Nery <vitorfelipenery@gmail.com>
2022-09-30 10:14:41 -0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ea575c7c-3bf8-4caf-b9fb-41755f2acde9';
+export const MODAL_CLOSE_SEC = 2.5;
